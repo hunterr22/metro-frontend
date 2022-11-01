33a1a1bb4621a0c3ccec2ad2488fdae883fc562c
hampton-black <hampton.j.black@gmail.com>
2022-11-01 16:21:43 -0400

    Update proxy IPs for Jenkins/K8s deployments

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,10 +5,10 @@ module.exports = function(app) {
     /*app.use(proxy('/api', { target: 'http://localhost:8080/' }));
     app.use(proxy('/account', { target: 'http://localhost:8081/' })); */
 	
-	app.use(proxy('/api', { target: 'http://172.19.0.2:8080/' }));
-	app.use(proxy('/account', { target: 'http://172.19.0.3:8081/' }));
+	/*app.use(proxy('/api', { target: 'http://172.19.0.2:8080/' }));
+	app.use(proxy('/account', { target: 'http://172.19.0.3:8081/' })); */
 
-/*     app.use(proxy('/api', { target: 'http://10.111.48.90:8080/' }));
-    app.use(proxy('/account', { target: 'http://10.104.111.92:8081/' })); */
+    app.use(proxy('/api', { target: 'http://10.110.142.67:8080/' }));
+    app.use(proxy('/account', { target: 'http://10.105.32.127:8081/' })); 
 
 };
