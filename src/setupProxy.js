f43bfde974c9b59f045fbe377d36be9c4cae4ed0
hampton-black <hampton.j.black@gmail.com>
2022-11-01 16:37:59 -0400

    Update proxy IP for metro-data

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -8,7 +8,7 @@ module.exports = function(app) {
 	/*app.use(proxy('/api', { target: 'http://172.19.0.2:8080/' }));
 	app.use(proxy('/account', { target: 'http://172.19.0.3:8081/' })); */
 
-    app.use(proxy('/api', { target: 'http://10.110.142.67:8080/' }));
+    app.use(proxy('/api', { target: 'http://10.104.210.234:8080/' }));
     app.use(proxy('/account', { target: 'http://10.105.32.127:8081/' })); 
 
 };
