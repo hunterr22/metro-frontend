4b8484a2a62091c86e77f6dd561360062ab33b9b
hampton-black <hampton.j.black@gmail.com>
2022-11-03 12:27:30 -0400

    Update env variables for IP addr

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -8,7 +8,7 @@ module.exports = function(app) {
 	/*app.use(proxy('/api', { target: 'http://172.19.0.2:8080/' }));
 	app.use(proxy('/account', { target: 'http://172.19.0.3:8081/' })); */
 
-    app.use(proxy('/api', { target: 'http://10.104.210.234:8080/' }));
-    app.use(proxy('/account', { target: 'http://10.105.32.127:8081/' })); 
+    app.use(proxy('/api', { target: 'http://10.110.250.46:8080/' }));
+    app.use(proxy('/account', { target: 'http://10.101.248.213:8081/' })); 
 
 };
